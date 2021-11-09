77e46af4125f72d2705669d971f71b620c6be22b
Tushkov Sergey <rabota054@mail.ru>
2021-11-10 00:22:40 +0500

    new files

---
+++ main.py
@@ -0,0 +1,9 @@
+from application import salary
+from application import people
+from datetime import date
+
+if __name__ == '__main__':
+    salary.calculate_salary()
+    people.get_employees()
+print(date.today())
+
